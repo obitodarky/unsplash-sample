75baf9e551dad418c1ea3d760b38ee16eba09e33
Harsh Mehta <deadspaceharsh@gmail.com>
2020-12-30 19:28:15 +0530

    hive initiated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:unsplash_sample/model/bookmark_images.dart';
 import 'package:unsplash_sample/ui/app_entry.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(BookmarkImagesAdapter());
   runApp(EntryWidget());
 }

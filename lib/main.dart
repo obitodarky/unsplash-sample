929c25a219ec447de09dcf2962c91b24512f52ce
Harsh Mehta <deadspaceharsh@gmail.com>
2020-12-30 11:35:29 +0530

    image list initial ui

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:unsplash_sample/ui/app_entry.dart';
+
+void main() {
+  runApp(EntryWidget());
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:unsplash_sample/bloc/image_list/image_list_bloc.dart';
+import 'package:unsplash_sample/bloc/image_list/index.dart';
+import 'package:unsplash_sample/model/photo_model.dart';
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-class Home extends StatelessWidget {
+class _HomeState extends State<Home> {
+  final _bloc = ImageListBloc();
+  final _scrollController = ScrollController();
+
+  final _scrollThreshold = 200.0;
+
+  @override
+  void initState() {
+    _scrollController.addListener(_onScroll);
+    _bloc.add(ImageFetched(1));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -19,12 +39,76 @@ class Home extends StatelessWidget {
         body: TabBarView(
           children: [
             //infinite list view
-            Container(color: Colors.red,),
+            BlocBuilder<ImageListBloc, ImageListState>(
+              cubit: _bloc,
+              builder: (buildContext, state) {
+                if (state is ImageError)
+                  return Center(
+                    child: Text("error"),
+                  );
+                if (state is InitialPhotoListState)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                if (state is ImageLoaded) {
+                  print(state.photos.length);
+                  return ListView.builder(
+                      itemCount: state.photos.length + 1,
+                      controller: _scrollController,
+                      itemBuilder: (buildContext, index) {
+                        if (index >= state.photos.length) return CircularProgressIndicator();
+                        Photo item = state.photos[index];
+                        double displayWidth = MediaQuery.of(context).size.width;
+                        double finalHeight =
+                            displayWidth / (item.width / item.height);
+
+                        return InkWell(
+                          onTap: () {
+                          },
+                          child: Hero(
+                            tag: "photo${item.id}",
+                            child: Stack(
+                              children: <Widget>[
+                                SizedBox(
+                                  width: displayWidth,
+                                  height: finalHeight,
+                                ),
+                                Image.network(
+                                  item.urls.thumb,
+                                  fit: BoxFit.fitWidth,
+                                  width: displayWidth,
+                                  height: finalHeight,
+                                ),
+                                Image.network(
+                                  item.urls.regular,
+                                  fit: BoxFit.fitWidth,
+                                  width: displayWidth,
+                                  height: finalHeight,
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                }
+
+                return Center(child: Text(""));
+              },
+            ),
             //bookmarks
             Container(color: Colors.purple),
           ],
         ),
       ),
     );
+  }
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      if(_bloc.state is ImageLoaded){
+        _bloc.add(ImageFetched((_bloc.state as ImageLoaded).page + 1));
+      }
+    }
   }
 }
